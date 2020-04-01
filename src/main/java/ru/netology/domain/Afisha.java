f4705769670995ae62c9fb6d84dd82ec7bd176ad
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-02 01:08:20 +0500

    changes added

---
+++ src/main/java/ru/netology/domain/Afisha.java
@@ -0,0 +1,61 @@
+package ru.netology.domain;
+
+public class Afisha {
+    private int id;
+    private String name;
+    private String imageUrl;
+    private String genre;
+    private int amountOfMovie = 10;
+
+    public Afisha() {
+    }
+
+    public Afisha(int id, String name, String imageUrl, String genre, int amountOfMovie) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.genre = genre;
+        this.amountOfMovie = amountOfMovie;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getAmountOfMovie() {
+        return amountOfMovie;
+    }
+
+    public void setAmountOfMovie(int amountOfMovie) {
+        this.amountOfMovie = amountOfMovie;
+    }
+}
+
---
+++ src/test/java/ru/netology/manager/AfishaManagerTest.java
@@ -0,0 +1,34 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Afisha;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class AfishaManagerTest {
+    private AfishaManager manager = new AfishaManager();
+    private Afisha movie1 = new Afisha(1, "movie1", "http://...", "thriller", 10);
+    private Afisha movie2 = new Afisha(2, "movie2", "http://...", "thriller", 10);
+    private Afisha movie3 = new Afisha(3, "movie", "http://...", "thriller", 10);
+
+    @BeforeEach
+    @Test
+    public void setUpManager() {
+        manager.add(movie1);
+        manager.add(movie2);
+        manager.add(movie3);
+    }
+
+    @Test
+    public void shouldGetAll() {
+        manager.getMovies();
+    }
+    @Test
+    public void shouldGetTenLasts() {
+        manager.getLastsTenAdded();
+        Afisha[] actual = manager.getMovies();
+        Afisha[] expected = new Afisha[]{movie3, movie2, movie1};
+        assertArrayEquals(expected,actual);
+    }
+}
