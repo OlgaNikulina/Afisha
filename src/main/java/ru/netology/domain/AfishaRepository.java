93c5ca8197fd51de47d238222d2a3d2055c0ec5a
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-02 12:56:45 +0500

    methods and tests added

--- src/main/java/ru/netology/domain/AfishaRepository.java
+++ src/main/java/ru/netology/domain/AfishaRepository.java
@@ -1,21 +1,50 @@
 package ru.netology.domain;
 
 public class AfishaRepository {
-    Afisha[] findAll() {
-        return new Afisha[0];
+    private Afisha[] movies = new Afisha[0];
+
+    public void save(Afisha movie) {
+        int length = movies.length + 1;
+        Afisha[] tmp = new Afisha[length];
+        System.arraycopy(movies, 0, tmp, 0, movies.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = movie;
+        movies = tmp;
     }
 
-    void save() {
+    public Afisha[] findAll() {
+        return movies;
     }
 
-    Afisha[] findById(int id) {
-        return new Afisha[0];
+    public void removeById(int id) {
+        int length = movies.length - 1;
+        Afisha[] tmp = new Afisha[length];
+        int index = 0;
+        for (Afisha movie : movies) {
+            if (movie.getId() != id) {
+                tmp[index] = movie;
+                index++;
+            }
+        }
+        movies = tmp;
     }
 
-    int removeById(int id) {
-        return id;
+    public Afisha[] findById(int id) {
+        int length = movies.length;
+        Afisha[] tmp = new Afisha[length];
+        int index = 0;
+        for (Afisha movie : movies) {
+            if (movie.getId() != id) {
+                tmp[index] = movie;
+                index++;
+            }
+        }
+        return new Afisha[index];
     }
 
-    void removeAll() {
+    public int[] removeAll() {
+        int [] movies = new int[0];
+        return movies;
     }
 }
+
--- src/main/java/ru/netology/manager/AfishaManager.java
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -5,34 +5,30 @@ import ru.netology.domain.AfishaRepository;
 
 public class AfishaManager {
     private AfishaRepository repository;
+
     public AfishaManager(AfishaRepository repository){
         this.repository = repository;
     }
 
-    private Afisha[] movies = new Afisha[0];
-
-    public void add (Afisha movie) {
-        int length =  movies.length + 1;
-        Afisha[] tmp = new Afisha[length];
-        for (int i = 0; i < movies.length ; i++) {
-            tmp[i] = movies[i];
-        }
-        int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = movie;
-        movies = tmp;
+    public void add(Afisha movie) {
+        repository.save(movie);
     }
 
-    public Afisha[] getLastsTenAdded() {
+    public Afisha[] getAll() {
+        Afisha[] movies = repository.findAll();
         Afisha[] result = new Afisha[movies.length];
-
-        for (int i = 0; i < result.length ; i++) {
-            int amountOfMovie = 10;
-            if (i < amountOfMovie) {
-                int index = movies.length - i - 1;
-                result[i] = movies[index];
-            }
+        for (int i = 0; i < result.length; i++) {
+            int index = movies.length - i - 1;
+            result[i] = movies[index];
         }
         return result;
+    }
 
+    public void removeById(int id) {
+        repository.removeById(id);
+    }
+
+    public void removeAll() {
+        repository.removeAll();
     }
 }
