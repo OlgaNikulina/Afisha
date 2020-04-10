89340cdd44dbc45dcd2c23661b84ed16e068a25f
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-10 23:27:32 +0500

    corrections done

--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -19,16 +19,16 @@ public class MovieManagerTest {
     @InjectMocks
     private MovieManager manager = new MovieManager(repository);
 
-    private Movie movie1;
-    private Movie movie2;
-    private Movie movie3;
-    private Movie movie4;
-    private Movie movie5;
-    private Movie movie6;
-    private Movie movie7;
-    private Movie movie8;
-    private Movie movie9;
-    private Movie movie10;
+    private Movie movie1 = new Movie(2, "movie2", "http://...", "thriller");
+    private Movie movie2 = new Movie(2, "movie2", "http://...", "thriller");
+    private Movie movie3 = new Movie(3, "movie3", "http://...", "thriller");
+    private Movie movie4 = new Movie(4, "movie4", "http://...", "thriller");
+    private Movie movie5 = new Movie(5, "movie5", "http://...", "thriller");
+    private Movie movie6 = new Movie(6, "movie6", "http://...", "thriller");
+    private Movie movie7 = new Movie(7, "movie7", "http://...", "thriller");
+    private Movie movie8 = new Movie(8, "movie8", "http://...", "thriller");
+    private Movie movie9 = new Movie(9, "movie9", "http://...", "thriller");
+    private Movie movie10 = new Movie(10, "movie10", "http://...", "thriller");
 
     @BeforeEach
     public void setUp() {
@@ -51,7 +51,7 @@ public class MovieManagerTest {
         manager.getLastsTenAdded(5);
 
         Movie[] expected = new Movie[]{movie5, movie4, movie3, movie2, movie1};
-        Movie[] actual = manager.getAll();
+        Movie[] actual = manager.getLastsTenAdded(5);
         assertArrayEquals(expected, actual);
     }
 
